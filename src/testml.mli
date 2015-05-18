3950059a2b85f361728e070b83e29ad25b0186c8
paul <paul@guerriere>
2015-05-17 22:46:39 -0400

    deleted:    src/foo 	deleted:    src/foo2 	deleted:    src/lib_openfd_ctypes.ml 	deleted:    src/lib_openfd_ctypes.mli 	deleted:    src/openfdlocks.ml 	deleted:    src/openfdlocks.mli 	deleted:    src/single_timed_lock.ml 	deleted:    src/single_timed_lock.mli 	deleted:    src/testml.ml 	deleted:    src/testml.mli 	deleted:    src/unit_test.ml

--- src/testml.mli
+++
@@ -1,9 +0,0 @@
-module FDL = Openfdlocks.OpenFDLocks
-module Testml :
-  sig
-    (*val filename : string*)
-    val exclusive_write : int -> Core.Std.Unix.File_descr.t -> unit
-    val athread : string -> int -> 'a -> int
-    val exec : arg:string -> unit
-    val command : Core.Std.Command.t
-  end
